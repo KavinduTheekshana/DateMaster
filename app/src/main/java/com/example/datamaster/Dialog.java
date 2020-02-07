30929bfe443932ad417298db1869784ff9c3ded6
Kavindu Theekshana <kavindutheekshana@gmail.com>
2020-02-07 23:29:51 +0530

    Mobitel Package Add

---
+++ app/src/main/java/com/example/datamaster/Dialog.java
@@ -0,0 +1,29 @@
+package com.example.datamaster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Dialog extends AppCompatActivity {
+    private Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dialog);
+        button=(Button) findViewById(R.id.dialog_back);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                HomeActivity();
+            }
+        });
+    }
+
+    public void HomeActivity(){
+        Intent intent = new Intent(this,HomeActivity.class);
+        startActivity(intent);
+    }
+}
--- app/src/main/java/com/example/datamaster/HomeActivity.java
+++ app/src/main/java/com/example/datamaster/HomeActivity.java
@@ -1,14 +1,42 @@
 package com.example.datamaster;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class HomeActivity extends AppCompatActivity {
+import com.google.android.material.card.MaterialCardView;
+
+import java.time.Instant;
+
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private CardView mobitel,dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        mobitel = (MaterialCardView) findViewById(R.id.btnMobitel);
+        dialog = (MaterialCardView) findViewById(R.id.btnDialog);
+        mobitel.setOnClickListener(this);
+        dialog.setOnClickListener(this);
+    }
+
+    public void mobitelView(View view){
+        startActivity(new Intent(this, Mobitel.class));
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent i;
+        switch (v.getId()){
+            case R.id.btnMobitel : i = new Intent(this,Mobitel.class);startActivity(i); break;
+            case R.id.btnDialog : i = new Intent(this,Dialog.class);startActivity(i);  break;
+            default:break;
+        }
+
     }
 }
